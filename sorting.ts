61f5671b7b6a818711552bfaab2c0ba6aa47a90b
Muriel Karkada <56532594+murielkarkada@users.noreply.github.com>
2020-09-08 13:34:36 +0530

    Delete sorting.ts

--- sorting.ts
+++
@@ -1,4 +0,0 @@
-var arr = new Array("Amit","Priya","Jhon","Anna","Zoe"); 
-console.log("Input array of names  : " + arr );
-var sorted = arr.sort(); 
-console.log("Names in sorted order : " + sorted );
