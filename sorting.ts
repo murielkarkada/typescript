c61954384d4a352c1db85e5cc5cdca045c2ebfa4
Muriel Karkada <56532594+murielkarkada@users.noreply.github.com>
2020-09-08 12:54:27 +0530

    Add files via upload

---
+++ sorting.ts
@@ -0,0 +1,4 @@
+var arr = new Array("Amit","Priya","Jhon","Anna","Zoe"); 
+console.log("Input array of names  : " + arr );
+var sorted = arr.sort(); 
+console.log("Names in sorted order : " + sorted );
---
+++ task3.ts
@@ -0,0 +1,22 @@
+var array :string[][]=[["Amit","Priya","Jhon"],["Znna","Zoe","Jack"],["Mary","Denver","Tesla"]]; 
+console.log("Input array of names  : " );
+for(let i=0;i<3;i++)
+{
+    console.log("team "+ ":" + array[i] )
+    }
+    var ele="Denver";
+for(let i=0;i<3;i++)
+{
+    for(let j=0;j<3;j++)
+    {
+        if(array[i][j]==ele)
+        { 
+          console.log("element "+ele+" found")
+          var flag=1;
+        
+        }
+        
+    }
+}
+if(flag!==1)
+console.log("element "+ele+" not found")
